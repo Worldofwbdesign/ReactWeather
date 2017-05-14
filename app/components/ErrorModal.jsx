f6d31cc3e6d583ed328c3c19cdfe2122cd781d5e
Worldofwbdesign <zavodzamutov@gmail.com>
2017-05-14 23:29:09 +0300

    last add files

---
+++ app/components/ErrorModal.jsx
@@ -0,0 +1,46 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+let ReactDOMServer = require('react-dom/server');
+
+var ErrorModal = React.createClass({
+
+  getDefaultProps() {
+    return {
+      title: 'Error'
+    }
+  },
+
+  propTypes: {
+    title: React.PropTypes.string,
+    message: React.PropTypes.string.isRequired
+  },
+
+  componentDidMount: function () {
+    let { title, message } = this.props;
+
+    let modalMarkup = (
+      <div id="error-modal" className="reveal tiny text-center" data-reveal="">
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>
+          <button className="button hollow" data-close="">
+            Okay
+          </button>
+        </p>
+      </div>
+    )
+
+    let $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+
+    var modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function () {
+    return (
+      <div></div>
+    )
+  }
+});
+
+module.exports = ErrorModal;
